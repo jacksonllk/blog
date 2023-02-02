45d3f7ed7c6a45ea19f365474483852e3b3649db
Jackson Ling <96425149+jacksonllk@users.noreply.github.com>
2023-02-02 16:21:29 +0800

    updated blog with local mongodb database

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -14,12 +15,33 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect(
+  "mongodb://localhost:27017/blogDB",
+  () => {
+    console.log("Connected to mongodb");
+  }
+);
+
+//post schema
+const postSchema = {
+  title: String,
+  content: String
+};
+
+//post model
+const Post = mongoose.model("Post", postSchema);
+
 let posts = [];
 
 app.get("/", function(req, res) {
-  res.render("home", {
-    startContent: homeStartingContent,
-    blogPosts: posts
+  Post.find({}, function(err, foundPost){
+    if (err) console.log(err);
+    else {
+      res.render("home", {
+        startContent: homeStartingContent,
+        blogPosts: foundPost
+      });
+    }
   });
 });
 
@@ -41,35 +63,25 @@ app.get("/compose", function(req, res) {
 
 app.get("/posts/:postId", function(req, res){
 
-  const requestedPostID = _.lowerCase(req.params.postId);
- 
-  posts.forEach(function(post){
-    const storedPostID = _.lowerCase(post.title);
-    if (requestedPostID === storedPostID) {
-      res.render("post", {
-        blogTitle: post.title,
-        blogContent: post.content
-      });
-    };
-  });
+  const requestedPostID = req.params.postId;
+
+  Post.findOne({_id:requestedPostID}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    })
+  })
 });
 
 app.post("/compose", function (req, res) {
-  const post = {
+  const post = new Post({
     title : req.body.blogTitle,
     content : req.body.blogPost
-  };
-  posts.push(post);
-  res.redirect("/");
-})
-
-
-
-
-
-
-
-
+  });
+  post.save(function(err){
+    if (!err) res.redirect("/");
+  });
+});
 
 
 
